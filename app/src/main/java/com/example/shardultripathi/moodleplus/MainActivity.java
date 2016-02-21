546d0b90938b9b35a257b85bd721fdc54f48819d
Shardul.Tripathi <shardul.511@gmail.com>
2016-02-21 20:17:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shardultripathi/moodleplus/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.shardultripathi.moodleplus;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText Username;
+    private EditText Password;
+    private Button Submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Username = (EditText)findViewById(R.id.username);
+        Password = (EditText)findViewById(R.id.password);
+        Submit = (Button)findViewById(R.id.submitbtn);
+
+        Submit.setOnClickListener(this);
+    }
+    public void login()
+    {
+        final String usrnm = Username.getText().toString().trim();
+        final String pswd = Password.getText().toString().trim();
+        String URL = "http://10.192.58.152:8000/default/login.json?userid="+usrnm+"&password="+pswd;
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, URL, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+                            if (response.getBoolean("success") == true) {
+                                Intent intent = new Intent(MainActivity.this, Courses.class);
+//                        intent.putExtra("response", response);
+//
+//                        intent.putExtra("teamname",ETteamname.getText().toString());
+//                        intent.putExtra("entry1",ETentry1.getText().toString());
+//                        intent.putExtra("name1",ETname1.getText().toString());
+//                        intent.putExtra("entry2",ETentry2.getText().toString());
+//                        intent.putExtra("name2", ETname2.getText().toString());
+//                        intent.putExtra("entry3",ETentry3.getText().toString());
+//                        intent.putExtra("name3",ETname3.getText().toString());
+//
+//                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+                            }
+                            else
+                                Toast.makeText(MainActivity.this, "Invalid login", Toast.LENGTH_LONG).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("username", usrnm);
+                params.put("password", pswd);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsObjRequest);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        if(v == Submit)
+        {
+            login();
+        }
+    }
+
+}
